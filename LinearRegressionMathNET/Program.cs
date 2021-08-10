e22e4932167e8f734bd5756a3caf0ddee3955d9a
I Putu Giovanni Eliezer <iputugiovanni@gmail.com>
2021-08-10 15:34:07 +0700

    update readme

---
+++ LinearRegressionMathNET/Program.cs
@@ -0,0 +1,22 @@
+﻿using MathNet.Numerics;
+using System;
+
+namespace LinearRegressionMathNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] xdata = new double[] { 5, 15, 25, 35, 45, 55 };
+            double[] ydata = new double[] { 5, 20, 14, 32, 22, 38 };
+
+            Tuple<double, double> p = Fit.Line(xdata, ydata);
+            double a = p.Item1; // 
+            double b = p.Item2; //
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Csharp_LinearRegression
+
+
+1. Without Library
+2. Using Math.NET Nuget Packages
